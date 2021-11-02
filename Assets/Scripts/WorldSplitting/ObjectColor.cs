f0ef51e9f0713087ae597bfe85f534c0b26c915a
JasonHo <jasonhojh1122@gmail.com>
2021-11-03 00:30:20 +0800

    Disable physics on split and merge

--- Assets/Scripts/WorldSplitting/ObjectColor.cs
+++ Assets/Scripts/WorldSplitting/ObjectColor.cs
@@ -7,12 +7,16 @@ public class ObjectColor : MonoBehaviour {
     [SerializeField] Dimension.Color color;
     Renderer _renderer;
     MaterialPropertyBlock _propBlock;
+    Collider col;
 
     private void Awake() {
         _propBlock = new MaterialPropertyBlock();
         _renderer = GetComponent<Renderer>();
         _renderer.GetPropertyBlock(_propBlock);
+        col = GetComponent<Collider>();
+        SetColor(color);
     }
+
     public void SetColor(Dimension.Color color) {
         this.color = color;
         _propBlock.SetColor("_Color", Dimension.MaterialColor[color]);
