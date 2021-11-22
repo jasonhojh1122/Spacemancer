c029027b09c09f3a07a3c04b6ff48e174c320b28
s950449 <b07902125@ntu.edu.tw>
2021-11-22 14:47:35 +0800

    Add skill color checking

--- Assets/Scripts/SkillController.cs
+++ Assets/Scripts/SkillController.cs
@@ -1,15 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Core;
 public class SkillController : MonoBehaviour
 {
     [SerializeField] SkillGameUIController skillGameUIController;
-    private void Update()
+    public Dimension.Color skillColor = Dimension.Color.NONE;
+    private Collider hitCollider = null;
+    private void FixedUpdate()
     {
         if (Input.GetButtonDown("Skill"))
         {
             skillGameUIController.gameObject.SetActive(true);
         }
         skillGameUIController.gameObject.SetActive(false);
+    }
+    private void withdraw()
+    {
+        ObjectColor color = hitCollider.GetComponent<ObjectColor>();
+        if (tryColor(color.Color,skillColor,0))
+        {
+
+        }
+    }
+    private void fill()
+    {
+        ObjectColor color = hitCollider.GetComponent<ObjectColor>();
+        if (tryColor(color.Color, skillColor, 1))
+        {
+
+        }
+    }
+    private bool tryColor(Dimension.Color objectColor,Dimension.Color targetColor,int mode)
+    {
+        bool ret = true;
+        Dimension.Color tmp;
+        switch (mode)
+        {
+            case 0:// withdraw mode
+                tmp = objectColor & targetColor;
+                if(tmp!=targetColor)
+                {
+                    ret = false;
+                }
+                break;
+            case 1: // fill mode
+            default:
+                tmp = objectColor | targetColor;
+                if(tmp == objectColor)
+                {
+                    ret = false;
+                }
+                break;
+        }
+        return ret;
     }
 }
