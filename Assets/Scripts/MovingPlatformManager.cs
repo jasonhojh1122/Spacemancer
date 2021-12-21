a46aa8a57b5a04638af35a432da176689bb84d96
s950449 <950449@hotmail.com.tw>
2021-12-21 17:58:40 +0800

    Tracking MovingPlatform in global manager

---
+++ Assets/Scripts/MovingPlatformManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MovingPlatformManager : MonoBehaviour
+{
+    ///<summary>
+    /// <c>List</c> of <c>MovingPlatforms' direction(MoveToEnd or not)</c>
+    ///</summary>
+    private List<bool> direction = new List<bool>();
+    ///<summary>
+    /// A tracking <c>List</c> of <c>MovingPlatform</c>
+    ///</summary>
+    [SerializeField] List<MovingPlatform> movingPlatforms;//Keep track of list of platforms
+
+    void Update(){
+        var i = 0;
+        foreach(MovingPlatform m in movingPlatforms){
+            direction.Add(m.moveToEnd);
+            m.ID = i;
+            m.isInit = true;
+            i++;
+        }
+    }
+    ///<summary>
+    /// Set MovingPlatform Direction of Specific ID
+    ///</summary>
+    /// <param name="id"> The MovingPlatform ID </param>
+    /// <param name="val"> MoveToEnd  </param>
+    public void SetDirection(int id,bool val){
+        direction[id] = val;
+    }
+    ///<summary>
+    /// Get MovingPlatform Direction of Specific ID
+    ///</summary>
+    /// <param name="id"> The MovingPlatform ID </param>
+    ///<returns> Direction Value</returns>
+    public bool getDirection(int id){
+        return direction[id];
+    }
+    
+}
