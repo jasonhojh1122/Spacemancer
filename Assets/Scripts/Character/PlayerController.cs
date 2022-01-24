89343df3456c7bb95b9730e971c394d1b783527e
JasonHo <jasonhojh1122@gmail.com>
2022-01-24 14:55:33 +0800

    Add camera movement/zoom control.

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine.InputSystem;
 
 namespace Character {
 
-    [RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
-    public class PlayerController : MonoBehaviour
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerController : InputController
     {
         [SerializeField] float maxSpeed;
         [SerializeField] float rotateSpeed;
@@ -13,58 +13,61 @@ namespace Character {
         [SerializeField] RuntimeAnimatorController walkAC;
 
         CharacterController characterController;
-        PlayerInput playerInput;
         InputAction moveAction;
 
         Animator animator;
         SplittablePlayer player;
         Vector3 vel;
         [SerializeField] bool moving;
-        [SerializeField] bool onLattice;
-        [SerializeField] bool onGround;
 
-        void Awake()
+        new void Awake()
         {
+            base.Awake();
             animator = GetComponent<Animator>();
             player = GetComponent<SplittablePlayer>();
-            playerInput = GetComponent<PlayerInput>();
             characterController = GetComponent<CharacterController>();
             moveAction = playerInput.actions["Movement"];
             animator.runtimeAnimatorController = idleAC;
             vel = Vector3.zero;
             moving = false;
-            onLattice = true;
         }
 
         protected void Update()
         {
+            if (pause) return;
             UpdateVelocity();
             UpdateMovingState();
             Move();
         }
 
-        void Move() {
-            onGround = characterController.SimpleMove(vel);
+        void Move()
+        {
+            characterController.SimpleMove(vel);
             transform.forward = Vector3.RotateTowards(transform.forward, vel, rotateSpeed * Time.deltaTime, 0.0f);
         }
 
-        void UpdateVelocity() {
+        void UpdateVelocity()
+        {
             var dir2d = moveAction.ReadValue<Vector2>();
             vel = new Vector3(dir2d.x, 0, dir2d.y) * maxSpeed;
-            if (Util.Fuzzy.CloseFloat(vel.magnitude, 0)) {
+            if (Util.Fuzzy.CloseFloat(vel.magnitude, 0))
                 vel = Vector3.zero;
-            }
         }
 
-        void UpdateMovingState() {
-            if (!Util.Fuzzy.CloseFloat(vel.magnitude, 0)) {
-                if (!moving) {
+        void UpdateMovingState()
+        {
+            if (!Util.Fuzzy.CloseFloat(vel.magnitude, 0))
+            {
+                if (!moving)
+                {
                     SetAnimatorController(walkAC);
                     moving = true;
                 }
             }
-            else {
-                if (moving) {
+            else
+            {
+                if (moving)
+                {
                     SetAnimatorController(idleAC);
                     moving = false;
                 }
